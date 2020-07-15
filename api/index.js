c088822b6b94c0631358ec152f9b3b07bf8a0247
神代綺凜 <i@loli.best>
2020-07-15 18:09:12 +0800

    test

--- api/index.js
+++ api/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
-const baiduocr = require('./src/ocr/baidu');
+const baiduocr = require('../serverless/ocr/baidu');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('<a href="/">Arknights Toolbox</a>');
 });
 
-app.post('/tagocr/baidu', async (req, res) => {
+app.post('/api/tagocr/baidu', async (req, res) => {
   const { image } = req.body;
   if (!image) res.json({ code: 403, msg: 'No image' });
   res.json(await baiduocr(image));
